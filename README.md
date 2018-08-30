34838ca48adb223b5d64cd9d94e30432ef7db42e
Alexei Ptitchkin <ptitchkin@gmail.com>
2018-08-30 07:58:32 -0400

    remove PartDetail.contextTypes.router:PropTypes from PartDetail

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 This is LearnAsYouGo scaffolding project for ReactJs app
 
+## v0.5
+- Add links with parameters to Part Detail Component
+- Add PartDetail Component
+- access url parameter from PartDetail Component 
+
+## v0.4
+- load dummy parts in PartsView.js
+- use App.js as layout inside router
+- do the routing in index.js and use links in navbar in App.js
+   
+## v_0.3
+- Add Router from bs Navigation
+
 ## v0.2 initial commit
 - extract PartsList to  partList.js
 
--- src/components/partDetail.js
+++ src/components/partDetail.js
@@ -1,15 +1,10 @@
 import React, { Component} from 'react';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-
-/*
-* {props.match.params.partId}
-*/
 
 const PartDetail = (props) => (
     <div>
         <div>
-            <h2>Part Detail {props.match.params.partId}</h2>
+            <h2>Part Detail</h2>  <h3>{props.match.params.partId}</h3>
             <input type="text" placeholder="your email" className="form-control"></input>
             <textarea type="text" placeholder="your message" className="form-control"></textarea>
             <button className="btn btn-primary">Save</button>
@@ -17,9 +12,5 @@ const PartDetail = (props) => (
         <Link to="/home">Back to list</Link>
     </div>
 );
-
-PartDetail.contextTypes = {
-    //NOT REQUIRED????  router: PropTypes.object.isRequired
-}
 
 export default PartDetail;
