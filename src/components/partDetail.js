edfeeac600efc71b10d9fc958e2805afb62a3342
Alexei Ptitchkin <ptitchkin@gmail.com>
2018-09-04 14:00:56 -0400

    That's really stupid way to handle input field in ReactJs forms

--- src/components/partDetail.js
+++ src/components/partDetail.js
@@ -11,6 +11,8 @@ class PartDetail extends Component {
             ROOT_URL: 'http://localhost:3000/parts/',
             part:{}
         };
+        //this.handlepPrtcdChange = this.handlepPrtcdChange.bind(this);
+        //after binding u can use in form `<input type="text" placeholder="pprtcd" className="form-control" value={`${this.state.part.pprtcd}`} onChange={this.handlepPrtcdChange}></input>`
     }
 
     componentDidMount() {
@@ -24,6 +26,13 @@ class PartDetail extends Component {
             .catch(error => console.log(error));
     }
 
+    handlepPrtcdChange(event) {
+        let part =  this.state.part;
+        part =  Object.assign({}, part, {pprtcd: event.target.value});
+        let state =  this.state;
+        state =  Object.assign({}, state, {part: part});
+        this.setState(state);
+    }
 
     render() {
         return (
@@ -33,7 +42,7 @@ class PartDetail extends Component {
                     <form action="#">
                         <input type="text" placeholder="multipleControl" className="form-control" ></input>  {this.state.part.multipleControl}
                         <input type="text" placeholder="returnControl" className="form-control" value={`${this.state.part.returnControl}`}></input>
-                        <input type="text" placeholder="pprtcd" className="form-control" value={`${this.state.part.pprtcd}`}></input>
+                        <input type="text" placeholder="pprtcd" className="form-control" value={`${this.state.part.pprtcd}`} onChange={this.handlepPrtcdChange.bind(this)}></input> {this.state.part.pprtcd}
                         <input type="text" placeholder="pprtn1" className="form-control" value={`${this.state.part.pprtn1}`}></input>
                         <input type="text" placeholder="pprtst" className="form-control" value={`${this.state.part.pprtst}`}></input>
                         <input type="text" placeholder="pcat" className="form-control" value={`${this.state.part.pcat}`}></input>
